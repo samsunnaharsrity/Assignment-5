1bc3d417fa33a3abb2d382b27106263f57c418f4
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-03-07 02:12:35 +0600

    first commit

---
+++ script/index.js
@@ -0,0 +1,27 @@
+
+//from er js
+const from = document.getElementById("sign-in").addEventListener("click",function () {
+    const inputName = document.getElementById("input-name");
+    const name = inputName.value;
+    //console.log(name);
+
+//pin
+const clickPassword  = document.getElementById("password")
+    const adminPassword = clickPassword.value;
+    //console.log(adminPassword);
+
+    
+if(name == "admin" && adminPassword == "admin123") {
+    alert("login")
+    window.location.assign("/homepage.html")
+
+}   
+else{
+    alert("not matched")
+    return
+}
+});
+
+//GitHub Issues Tracker
+
+console.log(hlw);
