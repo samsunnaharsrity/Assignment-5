e915252960660fc06186d9de2400dd16f420aa40
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-03-08 03:10:48 +0600

    modal done

--- script/homePage.js
+++ script/homePage.js
@@ -13,7 +13,7 @@ console.log(cards);
 
 cards.forEach((card) => {
      const allIssues = document.createElement("div")//${card.meaning} /${card.pronunciation}
-     allIssues.className ="bg-#EFEFEF p-5 rounded-md space-y-5 gap-10 shadow-xl border-t-5 border-green-500 "
+     allIssues.className ="bg-#EFEFEF   "
 
 
 
@@ -40,11 +40,13 @@ cards.forEach((card) => {
 // }
 // })
 
-
+ //onclick="my_modal_5.showModal()"
 
 
     allIssues.innerHTML =`
 
+           <div  onclick="loadModal(${card.id})" class="bg-#EFEFEF p-5 rounded-md space-y-5 gap-10 shadow-xl border-t-5 border-green-500 ">
+            
             <div class="flex justify-between">
                 <div><img src="${img}" alt="${card.status}"></div>
 
@@ -56,7 +58,7 @@ cards.forEach((card) => {
                 <p class="text-[#64748B] text-sm">${card.description}}</p>
             </div>
 
-            <div class="text-left">
+            <div  class="text-left">
                 <p class="btn btn-outline btn-error h-5  rounded-full"><i class="fa-solid fa-bug" style="color: rgb(241, 45, 14)"></i>Bug</p>
                 <p class="btn btn-outline btn-warning h-5 rounded-full"><i class="fa-solid fa-life-ring fa-xs" style="color: rgb(188, 154, 74);"></i></i>Help wanted</p>
             </div>
@@ -65,8 +67,10 @@ cards.forEach((card) => {
             <div class="text-[#64748B] text-sm">
                 <p>${card.createdAt}}</p>
                 <p>${card.updatedAt}}</p>
-            </div>    
-            
+            </div>
+           </div>
+              
+           
     `
 
     cardsIssues.appendChild(allIssues)
@@ -79,9 +83,7 @@ cards.forEach((card) => {
 //issues-sec
 // const issuesID = document.getElementById("issues-id")
 
-let all = document.querySelectorAll("all").length
-let open = document.querySelectorAll("open").length
-let closed = document.querySelectorAll("closed").length
+
 
 //  btns
 
@@ -108,4 +110,56 @@ function btns(id) {
     //console.log(clickedBtn);
 
    // console.log(id);
+}
+
+//modal
+
+const loadModal =async (id) => {
+  console.log(id);
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+  //console.log(url);
+  const res = await fetch(url)
+  const modalData = await res.json()
+   displayModal(modalData.data);
+}
+
+const displayModal =(modal)=>{
+    console.log(modal);
+    const modalDetails =document.getElementById("modal-details")
+    modalDetails.innerHTML=`   
+    <div id="modal-details" class="space-y-5 w-full">
+    <div class="gap-4">
+        <h2  class="font-semibold text-md">${modal.title}</h2>
+        <p class="bg-green-600 rounded-full w-[50px] text-center text-sm  text-white">${modal.status}</p>
+        <ul class="flex gap-5 text-sm">
+            <ol>Opened by Fahim Ahmed</ol>
+            <ol>22/02/2026</ol>
+        </ul>
+
+    </div>
+
+            <div class="text-left">
+                <p class="btn btn-outline btn-error h-5  rounded-full"><i class="fa-solid fa-bug" style="color: rgb(241, 45, 14)"></i>Bug</p>
+                <p class="btn btn-outline btn-warning h-5 rounded-full"><i class="fa-solid fa-life-ring fa-xs" style="color: rgb(188, 154, 74);"></i></i>Help wanted</p>
+            </div>
+            <div>
+                <p class="text-[#64748B] text-sm">${modal.description}}</p>
+            </div>
+
+            <div class="bg-#64748B w-full max-w-[636px] items-center flex justify-between shadow-md p-4 rounded-md">
+            
+                <p >Assignee : 
+                <span class="ml-1 font-bold "> <br> ${modal.assignee}</span>
+                </p>
+                
+                <p class="items-center gap-2"> Priority : <br> <span class="bg-red-600 text-sm text-white rounded-full  px-3 py-1 text-center">
+                ${modal.priority}</span>
+
+                </div>
+    </div>
+    `
+
+
+
+    document.getElementById("my_modal").showModal()
 }
